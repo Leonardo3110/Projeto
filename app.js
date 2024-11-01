8beb5933cc3f422709f14c746f42821db183a7a1
LeonardoAbelino <Leonardo.abelino@unidavi.edu.br>
2024-10-31 22:24:52 -0300

    .

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    if (username === "user" && password === "password") {
+      alert("Login bem-sucedido!");
+      window.location.href = "index.html";
+    } else {
+      alert("Credenciais incorretas. Tente novamente.");
+    }
+  });
+  
---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+      caches.open("site-static").then(function(cache) {
+        return cache.addAll([
+          "/",
+          "/index.html",
+          "/login.html",
+          "/style.css",
+          "/app.js",
+          "/manifest.json",
+          "/icon.png"
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
